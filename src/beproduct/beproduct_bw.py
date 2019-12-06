504599655333aee0511ffd7e433aaa6f6fe646ac
Yuri Golub <yuri.golub@winks.io>
2019-12-06 09:48:58 +0200

    Adding multiple file sync support

--- src/beproduct/beproduct_bw.py
+++ src/beproduct/beproduct_bw.py
@@ -3,6 +3,7 @@ import urllib.request
 import urllib.parse
 import urllib
 import config
+import os
 
 
 def __get_content__(url):
@@ -11,7 +12,7 @@ def __get_content__(url):
 
 class BeProductBW(BwApi.CallbackBase):
     def Run(self, garmentId, callbackId, dataString):
+        path_components = os.path.normpath(BwApi.GarmentPathGet(garmentId)).split(os.sep)
         BwApi.GarmentClose(garmentId, 1)
-        __get_content__(
-            config.BASE_URL + "api/bw/sync-back/" + config.USERID)
+        __get_content__(config.BASE_URL + "api/bw/sync-back/" + config.USERID + path_components[-3] + "/" + urllib.parse.quote_plus(path_components[-2]))
         return 0
