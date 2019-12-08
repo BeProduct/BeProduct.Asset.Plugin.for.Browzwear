ca1387bb560df44b1f2d92c53607f6c927ebbb54
Yuri Golub <yuri.golub@winks.io>
2019-12-08 08:37:43 +0200

    fixing bug

--- src/beproduct/beproduct_bw.py
+++ src/beproduct/beproduct_bw.py
@@ -14,5 +14,8 @@ class BeProductBW(BwApi.CallbackBase):
     def Run(self, garmentId, callbackId, dataString):
         path_components = os.path.normpath(BwApi.GarmentPathGet(garmentId)).split(os.sep)
         BwApi.GarmentClose(garmentId, 1)
-        __get_content__(config.BASE_URL + "api/bw/sync-back/" + config.USERID + path_components[-3] + "/" + urllib.parse.quote_plus(path_components[-2]))
+        if not config.USERID:
+            __get_content__(config.BASE_URL + "api/bw/sync-back/" + config.USERID)
+        else:
+            __get_content__(config.BASE_URL + "api/bw/sync-back/" + config.USERID + path_components[-3] + "/" + urllib.parse.quote_plus(path_components[-2]))
         return 0
